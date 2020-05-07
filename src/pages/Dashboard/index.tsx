b43ed89180ca7279d3982877294d5f70bd8aad1f
Ana Alice <anaalicefm22@gmail.com>
2020-05-06 22:05:39 -0300

    consultando api do gitgub

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,44 +1,73 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi'
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  }
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+    event.preventDefault();
+
+    if (!newRepo) {
+      setInputError('Digite o autor/nome do repositório.')
+    }
+
+    try {
+      const response = await api.get<Repository>(`/repos/${newRepo}`);
+
+      const repository = response.data
+
+      setRepositories([...repositories, repository])
+      setNewRepo('')
+    } catch (err) {
+      setInputError('Erro ao buscar repositório.')
+    }
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
 
-      <Form>
-        <input placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img src="https://avatars2.githubusercontent.com/u/29892114?s=460&u=8b5cb2df315a4abe25cbe93612c2e11f6cc0f5b3&v=4"
-           alt="Ana Alice"
-          />
-          <div>
-            <strong>anaalicefig/tsnode-deposit</strong>
-            <p>Base project to be used in projects using TypeScript with NodeJS and MongoDB</p>
-          </div>
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img src="https://avatars2.githubusercontent.com/u/29892114?s=460&u=8b5cb2df315a4abe25cbe93612c2e11f6cc0f5b3&v=4"
-           alt="Ana Alice"
-          />
-          <div>
-            <strong>anaalicefig/tsnode-deposit</strong>
-            <p>Base project to be used in projects using TypeScript with NodeJS and MongoDB</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
